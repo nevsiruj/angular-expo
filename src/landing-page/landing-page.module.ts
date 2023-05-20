290aba748f37a327b3ef19418a8df1f280a8a2b1
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-20 12:49:11 -0300

    1 file modified

--- src/landing-page/landing-page.module.ts
+++ src/landing-page/landing-page.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PanelModule } from 'primeng/panel';
 import { LandingPageComponent } from './landing-page.component'; // Importa el componente LandingPageComponent
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, PanelModule],
+  imports: [CommonModule],
 })
 export class LandingPageModule {}
