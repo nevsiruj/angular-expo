4e53e0c6804e7b3ee12e4265d97a7fde6ab5e191
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-20 12:51:50 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // import { LandingPageComponent } from '../landing-page/landing-page.component'; // Importa el componente LandingPageComponent
 import { LandingPageModule } from '../landing-page/landing-page.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,5 +9,6 @@ import { AppComponent } from './app.component';
   imports: [BrowserModule, LandingPageModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
