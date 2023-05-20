8fc7339ace783ce8e14537e123ff0ae0a736645e
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-20 12:58:05 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
