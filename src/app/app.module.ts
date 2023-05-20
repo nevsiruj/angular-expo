30140abd3d9fc91587f7fa8d72bdd9b9363be485
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-20 12:54:39 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 // import { LandingPageComponent } from '../landing-page/landing-page.component'; // Importa el componente LandingPageComponent
-import { LandingPageModule } from '../landing-page/landing-page.module';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+// import { LandingPageComponent } from '../landing-page/landing-page.component';
+// import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [AppComponent], // Agrega LandingPageComponent en las declarations
+//   imports: [BrowserModule, LandingPageComponent],
+//   providers: [],
+//   bootstrap: [AppComponent],
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+// })
+// export class AppModule {}
+
+// import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent], // Agrega LandingPageComponent en las declarations
-  imports: [BrowserModule, LandingPageModule],
+  declarations: [
+    LandingPageComponent,
+    // other components here
+  ],
+  imports: [
+    BrowserModule,
+    // other modules here
+  ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [LandingPageComponent],
 })
 export class AppModule {}
