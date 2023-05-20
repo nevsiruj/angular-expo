7a6515d261507bba62f7e21f5bfe9625326429ff
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-20 16:53:21 -0300

    2 files modified

--- src/contact-form/contact-form.component.ts
+++ src/contact-form/contact-form.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      message: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  submitForm(): void {
+    if (this.contactForm.valid) {
+      const formValues = this.contactForm.value;
+      console.log('Form Values:', formValues);
+      // Aquí puedes añadir la lógica para enviar estos datos a tu servidor o servicio de email.
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
