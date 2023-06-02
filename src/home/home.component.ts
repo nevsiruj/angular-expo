da3d8d1f19672653c0ef83e1f2f2cd911dfd1886
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-06-02 01:08:17 -0300

    1 binaries uploaded

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
@@ -16,7 +16,51 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ],
 })
 export class HomeComponent implements OnInit {
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
+
+  private intervalId: any;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateCountdown();
+
+    // Actualizar el contador cada segundo
+    this.intervalId = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    // Limpiar el intervalo cuando el componente se destruya
+    clearInterval(this.intervalId);
+  }
+
+  private updateCountdown() {
+    const currentDate = new Date();
+    const targetDate = new Date('August 19, 2023 00:00:00');
+
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+
+    if (timeDifference > 0) {
+      this.days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      this.minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      this.seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    } else {
+      // El tiempo objetivo ha pasado, establecer todas las unidades a 0
+      this.days = 0;
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+      clearInterval(this.intervalId); // Detener el intervalo cuando el tiempo objetivo ha pasado
+    }
+  }
 }
