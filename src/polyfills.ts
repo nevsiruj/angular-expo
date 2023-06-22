24b2d299d3647842aa942e363c5f9632128c362e
ignaciottosolini <ignaciottosolini@gmail.com>
2023-06-22 15:20:53 -0300

    v1

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,44 +1,68 @@
 /**
- * Este archivo incluye los polyfills necesarios por Angular y se carga antes de la aplicación.
- * Puedes agregar tus propios polyfills adicionales a este archivo.
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
  *
- * Este archivo está dividido en 2 secciones:
- *   1. Polyfills de navegador. Estos se aplican antes de cargar ZoneJS y están ordenados por navegadores.
- *   2. Importaciones de la aplicación. Archivos importados después de ZoneJS que deberían cargarse antes de tu archivo principal.
+ * This file is divided into 2 sections:
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ *      file.
  *
- * La configuración actual es para los llamados navegadores "evergreen"; las últimas versiones de los navegadores que
- * se actualizan automáticamente. Esto incluye Safari >= 10, Chrome >= 55 (incluyendo Opera),
- * Edge >= 13 en el escritorio, y iOS 10 y Chrome en móviles.
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
- * Más información en https://angular.io/guide/browser-support
+ * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
-   
-/***************************************************************************************************
- * POLYFILLS DE NAVEGADOR
+
+/*********************************
+ * BROWSER POLYFILLS
  */
-   
-/** IE10 y IE11 requiere lo siguiente para soporte NgClass en elementos SVG */
-// import 'classlist.js';  // Ejecuta `npm install --save classlist.js`.
-   
+
+/* IE9, IE10 and IE11 requires all of the following polyfills. */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+/** IE10 and IE11 requires the following to support `@angular/animation`. */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+
+/* Evergreen browsers require these. */
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+
+
+/* ALL Firefox browsers require the following to support `@angular/animation`. */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+
+
+/*********************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+
+
+/*********************************
+ * APPLICATION IMPORTS
+ */
+
 /**
- * Animaciones Web `@angular/platform-browser/animations`
- * Solo requerido si AnimationBuilder se usa en la aplicación y usando IE/Edge o Safari.
- * El soporte de animación estándar en Angular NO requiere ningún polyfill (a partir de Angular 6.0).
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'web-animations-js';  // Ejecuta `npm install --save web-animations-js`.
-   
-/***************************************************************************************************
- * Zone JS es requerido por Angular mismo.
- */
-import 'zone.js/dist/zone';  // Incluido con Angular CLI.
-   
-   
-/***************************************************************************************************
- * IMPORTACIONES DE LA APLICACIÓN
- */
-   
-/**
- * Tuberías de fecha, moneda, decimal y porcentaje.
- * Necesario para: Todos excepto Chrome, Firefox, Edge, IE11 y Safari 10
- */
-// import '@angular/common/locales/global/es'; // Descomenta esta línea si necesitas soporte de localización global para español
+// import 'intl';  // Run `npm install --save intl`.
