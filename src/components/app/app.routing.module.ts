34d7dc8f6fe59093845b7dab05b6c2d8686e24fb
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-06-27 19:34:53 -0300

    11 files modified, 1 added

--- src/components/app/app.routing.module.ts
+++ src/components/app/app.routing.module.ts
@@ -4,12 +4,14 @@ import { ContactFormComponent } from '../contact-form/contact-form.component';
 import { HomeComponent } from '../home/home.component';
 import { QuienesSomosComponent } from '../quienes-somos/quienes-somos.component';
 import { SponsorComponent } from '../sponsor/sponsor.component';
+import { ParticipantesComponent } from '../participantes/participantes.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quienessomos', component: QuienesSomosComponent },
   { path: 'sponsor', component: SponsorComponent },
   { path: 'contacto', component: ContactFormComponent },
+  { path: 'participantes', component: ParticipantesComponent },
 ];
 
 @NgModule({
